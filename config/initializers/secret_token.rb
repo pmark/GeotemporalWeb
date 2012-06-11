21f18f0237841696b57d6a6863591d8858bc8a70
P. Mark Anderson <mark@bordertownlabs.com>
2012-06-10 21:42:24 -0700

    import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geotemporal::Application.config.secret_token = 'ce867efba476d4e6eafe921f5dc8dfa560bf8c6bb7e53c605b7e491451e77640994666ca3bacbfcc8a773d1749ef51a21b96df646955a511d49ed71916c0b067'
